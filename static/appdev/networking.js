fccd1a5d990a121fd2cc77c3d9f82951e72a6d05
Fabian Pegel <git@fabianpegel.de>
2024-07-19 19:03:50 +0200

    fix playerListUpdate typo

--- static/appdev/networking.js
+++ static/appdev/networking.js
@@ -1,6 +1,6 @@
 var socket = new window.WebSocket("wss://" + window.location.host);
 
-socket.onmessage = function(event) {
+socket.onmessage = function (event) {
   console.log("WebSocket message received:", event.data);
   let message = JSON.parse(event.data);
   switch (message.msgType) {
@@ -17,16 +17,20 @@ socket.onmessage = function(event) {
       break;
     case 'assignGunID':
       playerGameData.gunID = message.GunID;
+      break;
     case 'updateWeaponDefinitions':
       weaponDefinitions = message.weapons;
-    case 'playerListUpdated':
+      break;
+    case 'playerListUpdate':
       playerList = message.players;
+      break;
     case 'kill':
       enemyKilled();
+      break;
   }
 };
 
-let initMsg = { "msgType":"join" }
+let initMsg = { "msgType": "join" }
 socket.onopen = () => {
   socket.send(JSON.stringify(initMsg));
 };

bafb30ac5c7f205350f9ddbd69a52a790573dbfd
davenewham <davidpaulnewham@gmail.com>
2024-08-17 00:23:15 +0200

    typescriptify audio.ts

--- src/audio.ts
+++ src/audio.ts
@@ -1,40 +1,36 @@
-var audioContext = null;
-var gunSounds = {};
-const SOUND_GUNSHOT = "gunshot"
-const SOUND_RELOAD = "reload"
-/**
- * @typedef {SOUND_GUNSHOT | SOUND_RELOAD} Sound
- */
+// todo: turn this into a class
 
-function ensureAudioContext() {
+type Sound = "gunshot" | "reload"
+
+var audioContext: AudioContext = null;
+const gunSounds: Record<Sound, AudioBuffer | null> = {
+  gunshot: null,
+  reload: null,
+};
+
+function ensureAudioContext(): void {
   // only load the audioContext after user interaction, otherwise we won't get one.
   if (audioContext === null) {
     audioContext = new AudioContext();
   }
 }
 
-/**
- * @param {Sound} sound
- */
-function playSound(sound) {
+function playSound(sound: Sound) {
   ensureAudioContext();
-  var source = audioContext.createBufferSource();
+
+  const source = audioContext!.createBufferSource();
   source.buffer = gunSounds[sound];
   source.connect(audioContext.destination);
   source.start(0);
 }
 
-/**
- * @param {Sound} name
- * @param {string} url
- */
-function loadSound(name, url) {
+function loadSound(name: Sound, url: string): void {
   ensureAudioContext();
-  var request = new XMLHttpRequest();
+  const request = new XMLHttpRequest();
   request.open('GET', url, true);
   request.responseType = 'arraybuffer';
   request.onload = function () {
-    audioContext.decodeAudioData(request.response, function (buffer) {
+    audioContext!.decodeAudioData(request.response, function (buffer) {
       gunSounds[name] = buffer;
     }, (error) => { console.log(error) });
   }

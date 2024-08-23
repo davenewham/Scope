89afd395230c85b34b68aec22df9db312dab0295
Andi Theke <andreas-dietrich97@web.de>
2024-08-23 17:44:00 +0200

    Changed: all lambda functions in networking.js are now actual functions to allow for reconnection Added: a reconnection function that will be triggered when connection is lost.

--- static/appdev/networking.js
+++ static/appdev/networking.js
@@ -1,6 +1,7 @@
 var socket = new window.WebSocket("wss://" + window.location.host);
+var timer = null;
 
-socket.onmessage = function (event) {
+function onMessage(event) {
   console.log("WebSocket message received:", event.data);
   let message = JSON.parse(event.data);
   switch (message.msgType) {
@@ -28,9 +29,34 @@ socket.onmessage = function (event) {
       enemyKilled();
       break;
   }
-};
+}
 
-let initMsg = { "msgType": "join" }
-socket.onopen = () => {
+let initMsg = { "msgType": "join" };
+function onOpen() {
+  clearInterval(timer);
+  timer = null;
   socket.send(JSON.stringify(initMsg));
-};
+}
+
+function onClose() {
+  console.log("WebSocket closed, needing for reconnection.");
+  if (timer === null) {
+    timer = setInterval(() => {
+      reconnect();
+    }, 3000);
+  }
+}
+
+function reconnect() {
+  console.log("Trying to reconnect to server ...")
+  socket = new window.WebSocket("wss://" + window.location.host);
+  connect();
+}
+
+function connect() {
+  socket.onmessage = onMessage;
+  socket.onopen = onOpen;
+  socket.onclose = onClose;
+}
+
+connect();
